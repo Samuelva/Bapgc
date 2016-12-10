0845efa7183ce777e5d8f0981f799b475d62cc8b
samuelva <samuelvanapeldoorn@outlook.com>
2016-12-10 14:41:03 +0100

    Tabmenu en statistiekgedeelte zijn schaalbaar

--- App/Vergelijken.java
+++ App/Vergelijken.java
@@ -3,8 +3,13 @@ package sample;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
+import javax.tools.Tool;
+import java.awt.*;
+import java.applet.*;
+import java.util.PrimitiveIterator;
 
-public class Vergelijken extends Pane {
+
+public class Vergelijken extends StackPane {
     TabPane tabPane;
     Tab testTab;
     Tab moduleTab;
@@ -14,36 +19,55 @@ public class Vergelijken extends Pane {
     HBox moduleTabBox = new HBox();
     HBox periodTabBox = new HBox();
 
+    Keuzemenu testChoiceMenu;
+    Keuzemenu moduleChoiceMenu;
+    Keuzemenu periodChoiceMenu;
+
+    Statistiek testStatistics;
+    Statistiek moduleStatistics;
+    Statistiek periodStatistics;
+
 
     public Vergelijken(){
         createTabs();
+        createChoiceMenus();
+        createStatistics();
 
-        Keuzemenu testChoiceMenu = new Keuzemenu();
-        Keuzemenu moduleChoiceMenu = new Keuzemenu();
-        Keuzemenu periodChoiceMenu = new Keuzemenu();
+        BorderPane test1 = new BorderPane();
+        test1.setTop(testChoiceMenu.getTestMenu());
+        test1.setBottom(testChoiceMenu.getAnderMenu());
 
-        BorderPane toetsMenuBox = testChoiceMenu.getTestMenu();
-        BorderPane moduleMenuBox = moduleChoiceMenu.getModuleMenu();
-        BorderPane periodMenuBox = periodChoiceMenu.getPeriodMenu();
-        toetsMenuBox.setMinHeight(400);
-
-        Statistiek testStatistics = new Statistiek();
-        Statistiek moduleStatistics = new Statistiek();
-        Statistiek periodStatistics = new Statistiek();
-
-        testTabBox.getChildren().addAll(toetsMenuBox, testStatistics.returnStatisticsBox());
-        moduleTabBox.getChildren().addAll(moduleMenuBox, moduleStatistics.returnStatisticsBox());
-        periodTabBox.getChildren().addAll(periodMenuBox, periodStatistics.returnStatisticsBox());
+        testTabBox.getChildren().addAll(test1, testStatistics.returnStatisticsBox());
+        moduleTabBox.getChildren().addAll(moduleChoiceMenu.getModuleMenu(), moduleStatistics.returnStatisticsBox());
+        periodTabBox.getChildren().addAll(periodChoiceMenu.getPeriodMenu(), periodStatistics.returnStatisticsBox());
 
         testTab.setContent(testTabBox);
         moduleTab.setContent(moduleTabBox);
         periodTab.setContent(periodTabBox);
+
+//        VBox.setVgrow(test1, Priority.ALWAYS);
+//        VBox.setVgrow(testTabBox, Priority.ALWAYS);
+//        VBox.setVgrow(test2, Priority.ALWAYS);
+
+//        testje.getChildren().add(tabPane);
+//        HBox.setHgrow(testTabBox, Priority.ALWAYS);
+//        testTabBox.setMaxWidth(Double.MAX_VALUE);
+//        HBox.setHgrow(tabPane, Priority.ALWAYS);
+//        tabPane.setMaxWidth(Double.MAX_VALUE);
+//        HBox.setHgrow(testje, Priority.ALWAYS);
+//        testje.setMaxWidth(Double.MAX_VALUE);
 
         this.getChildren().add(tabPane);
 
         testStatistics.addStatistics(new int[] {40, 50, 50, 30});
         testStatistics.addStatistics(new int[] {30, 20, 60, 70});
         testStatistics.addStatistics(new int[] {50, 80, 60, 40});
+//        testStatistics.addStatistics(new int[] {40, 50, 50, 30});
+//        testStatistics.addStatistics(new int[] {30, 20, 60, 70});
+//        testStatistics.addStatistics(new int[] {50, 80, 60, 40});
+//        testStatistics.addStatistics(new int[] {40, 50, 50, 30});
+//        testStatistics.addStatistics(new int[] {30, 20, 60, 70});
+//        testStatistics.addStatistics(new int[] {50, 80, 60, 40});
 
         moduleStatistics.addStatistics(new int[] {11, 46, 23 ,77});
         moduleStatistics.addStatistics(new int[] {33, 6, 43, 6});
@@ -67,7 +91,20 @@ public class Vergelijken extends Pane {
         tabPane.getTabs().add(testTab);
         tabPane.getTabs().add(moduleTab);
         tabPane.getTabs().add(periodTab);
+
         HBox.setHgrow(tabPane, Priority.ALWAYS);
+    }
+
+    private void createChoiceMenus() {
+        testChoiceMenu = new Keuzemenu();
+        moduleChoiceMenu = new Keuzemenu();
+        periodChoiceMenu = new Keuzemenu();
+    }
+
+    private void createStatistics() {
+        testStatistics = new Statistiek();
+        moduleStatistics = new Statistiek();
+        periodStatistics = new Statistiek();
     }
 
 }
