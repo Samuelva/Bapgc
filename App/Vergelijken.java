7f75196c81d676abba34203c4befca9033ef9306
samuelva <samuelvanapeldoorn@outlook.com>
2016-12-14 12:35:16 +0100

    Revert "Testcommit"
    This reverts commit 21a7083c204cfb5e42eda49f609ae49fee12c713.

--- App/Vergelijken.java
+++ App/Vergelijken.java
@@ -89,7 +89,7 @@ public class Vergelijken extends StackPane {
         // Goedkope fix
         // Vergelijkscherm past zich niet aan aan de veticale hoogte van de applet voor
         // een of andere reden.
-        tabPane.setPrefHeight(500);
+        tabPane.setPrefHeight(1080);
         VBox.setVgrow(tabPane, Priority.ALWAYS);
     }
 
