033a407958e762fb55abd25bc9d2bd6d4744e151
Tim <timo9518@hotmail.com>
2017-01-18 15:36:49 +0100

    small change in get toetsid

--- Die app map/src/database/VoorbeeldAanroep.java
+++ Die app map/src/database/VoorbeeldAanroep.java
@@ -8,17 +8,18 @@ import java.nio.file.Paths;
 public class VoorbeeldAanroep {
     public static void main(String[] args) {
         DatabaseConn d = new DatabaseConn();
-        d.InputModule("Bapgc", "jemama", 8);
-        d.InputToets("2016","3","2","Bapgc","Toets","1",70, 30);
-        new Reader(Paths.get("src/brela_1e_1617.csv").toString(), d.GetToetsID("Bapgc", "2016", "3", "2", "1"));
+        //d.InputModule("Bapgc", "jemama", 8);
+        //d.InputToets("2016","3","2","Bapgc","Toets","1",70, 30);
+        //new Reader(Paths.get("src/brela_1e_1617.csv").toString(), d.GetToetsID("Bapgc", "2016", "3", "2", "1"));
         //d.database.InputVraag("2a", 10, 1, true);
         //d.database.InputStudent(1088948,"Tim", "Bin3b");
         //d.InputScore(1088948, 1, 10);
         //String[][] table = d.GetStudentScores(d.GetToetsID("Bapgc", "2016", "3", "2", "1"));
-        //String[] table = d.GetCesuurMaxGok(d.GetToetsID("Bapgc", "2016", "3", "2", "1"));
+        //String[] table = d.GetCesuurMaxGok(d.GetToetsID("Bapgc", "2016", "3", "2", "1", "Toets"));
         //for (String y : table) {
         //    System.out.println(y);
         //}
+        //System.out.println(d.GetTable("MODULE").toString());
         d.CloseConnection();
     }
 }
