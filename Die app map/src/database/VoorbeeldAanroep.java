edd5317efa20b0a0afee87edcdecf73e022421eb
DavyCats <davycats.dc@gmail.com>
2017-01-20 22:11:03 +0100

    Tabel vullen werkend gemaakt, tabel bij invoeren editable gemaakt, bug fix

--- Die app map/src/database/VoorbeeldAanroep.java
+++ Die app map/src/database/VoorbeeldAanroep.java
@@ -8,13 +8,18 @@ import java.nio.file.Paths;
 public class VoorbeeldAanroep {
     public static void main(String[] args) {
         DatabaseConn d = new DatabaseConn();
-        //d.InputModule("Bapgc", "jemama", 8);
-        //d.InputModule("Bacf", "jepapa", 8);
-        //d.InputToets("2016","3","2","Bapgc","Praktijk","2",70, 30);
+        d.InputModule("Bapgc", "jemama", 8);
+        d.InputModule("Bacf", "jepapa", 8);
+        d.InputToets("2016","3","2","Bapgc","Praktijk","2",70, 30);
         //new Reader(Paths.get("src/brela_1e_1617.csv").toString(), d.GetToetsID("Bapgc", "2016", "3", "2", "1", "Toets"));
-        //d.InputVraag("2a", 10, 1, true);
-        //d.database.InputStudent(1088948,"Tim", "Bin3b");
-        //d.InputScore(1088948, 1, 10);
+        d.InputVraag("2a", 10, 1, true);
+        d.InputVraag("2b", 10, 1, true);
+        d.InputStudent(1088948,"Tim", "Bin3b");
+        d.InputStudent(1000000,"noname", "Bin3b");
+        d.InputScore(1088948, 1, 10);
+        d.InputScore(1088948, 2, 5);
+        d.InputScore(1000000, 1, 10);
+        d.InputScore(1000000, 2, 8);
         //String[][] table = d.GetStudentScores(d.GetToetsID("Bapgc", "2016", "3", "2", "1"));
         //String[] table = d.GetCesuurMaxGok(d.GetToetsID("Bapgc", "2016", "3", "2", "1", "Toets"));
         //Object[][] table = d.GetVragenVanToets(1);
