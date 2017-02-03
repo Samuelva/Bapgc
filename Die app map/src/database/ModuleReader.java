b9aaee3e6edea24173d09b747fa10741832f30ff
Midas <Midas_kuilman@hotmail.com>
2017-02-03 18:52:25 +0100

    Spatie probleem gefixt en start aan documentatie

--- Die app map/src/database/ModuleReader.java
+++ Die app map/src/database/ModuleReader.java
@@ -13,8 +13,15 @@ import java.util.List;
  * Created by midas on 18-1-2017.
  */
 public class ModuleReader {
+    /**
+     * Deze functie maakt connectie met de database. Er wordt een CSV
+     * ingelezen en ingevoerd in de database. Eerst worden alle
+     * modullen ingevoerd en vervolgens worden alle toetsen uit de CSV
+     * file daaraan gekoppeld. Dit wordt gedaan voor alle drie de
+     * gelegenheden en voor alle soorten toetsvormen.
+     * @param csvFile
+     */
     public ModuleReader(String csvFile){
-        //csvFile = "C:/Users/midas/Desktop/Githup/Bapgc/Die app map/src/modulesCSV.csv";
         BufferedReader br = null;
         String line = "";
         ArrayList<String[]> modules = new ArrayList<>();
@@ -23,33 +30,30 @@ public class ModuleReader {
         gelegenheid.add("1");
         gelegenheid.add("2");
         gelegenheid.add("3");
-
         DatabaseConn d = new DatabaseConn();
+
         try {
             br = new BufferedReader(new FileReader(csvFile));
             line = br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] lijn = line.split(";");
                 modules.add(lijn);
-                //d.InputModule("Bapgc", "jemama", 8);
-                //d.InputToets("2016","3","2","Bapgc","Toets","1",70, 30);
             }
                 for (int i = 0; i < modules.size(); i++) {
                     d.InputModule(modules.get(i)[1], modules.get(i)[1], Integer.parseInt(modules.get(i)[2]));
                 }
             for (int i = 0; i < modules.size(); i++) {
-
-                //Get moduleCode
                 for (int j = 0; j < gelegenheid.size(); j++) {
                     for (int n = 0; n < modules.get(i)[3].toString().split(",").length; n++) {
                         d.InputToets(modules.get(i)[0],
-                                modules.get(i)[4],
+                                modules.get(i)[4].replaceAll(" ", ""),
                                 modules.get(i)[5],
                                 modules.get(i)[1],
                                 modules.get(i)[3].toString().split(",")[n],
                                 gelegenheid.get(j),
                                 0,
                                 0);
+
                     }
                 }
             }
--- Die app map/src/database/Reader.java
+++ Die app map/src/database/Reader.java
@@ -10,59 +10,53 @@ import java.util.ArrayList;
  * Created by midas on 16-1-2017.
  */
 public class Reader {
-
+    /**
+     * Deze functie maakt connectie met de database. Er wordt een CSV
+     * ingelezen en ingevoerd in de database. Eerst worden alle
+     * vragen ingevoerd en vervolgens worden alle resultaten uit de CSV
+     * file daaraan gekoppeld.
+     * @param csvFile
+     * @param toetsID
+     */
     public Reader(String csvFile, int toetsID) {
-
         BufferedReader br = null;
         String line = "";
         String deelvraag = "";
         String meerekenen = "";
         String punten = "";
         ArrayList<String[]> scores = new ArrayList<>();
-
         DatabaseConn d = new DatabaseConn();
 
         try {
-
             br = new BufferedReader(new FileReader(csvFile));
-
             deelvraag = br.readLine();
             meerekenen = br.readLine();
             punten = br.readLine();
 
             while ((line = br.readLine()) != null) {
-
                 String[] lijn = line.split(";");
-
                 scores.add(lijn);
             }
             for(int i=2; i<deelvraag.split(";").length; i++){
-
                 d.InputVraag(
                         deelvraag.split(";")[i],
                         Integer.parseInt(punten.split(";")[i]),
                         toetsID,
                         Boolean.valueOf(meerekenen.split(";")[i]));
             }
-
             for(int i=0; i<scores.size(); i++){
                 d.InputStudent(
                         Integer.parseInt(scores.get(i)[0]),
                         "Naam",
-                        scores.get(i)[1])
-                ;
+                        scores.get(i)[1]);
                 for(int j=2; j<deelvraag.split(";").length; j++){
-
                     int VraagID = d.GetVraagID(deelvraag.split(";")[j], toetsID);
                     d.InputScore(
                             Integer.parseInt(scores.get(i)[0]),
                             VraagID,
                             Integer.parseInt(scores.get(i)[j]));
                 }
-
-
             }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
